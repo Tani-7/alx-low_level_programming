20a4cb9b84a893fbf2ca4f7ed6b5f065728afbf0
Tani-7 <justomwansa@outlook.com>
2022-09-20 20:39:53 -0700

    C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
 /**
- * 	main - Entry point
+ *	main - Entry point
  *
- * 	Description: prints "Programming is like building a multilingual puzzle
+ *	description: prints "Programming is like building a multilingual puzzle
  *
- *   	Return: Always 0 (Success)
+ *	return: Always 0 (Success)
  */
 
 int main(void)
 
 
-{
-
+{	
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
-
-
+	return (0)
 }
