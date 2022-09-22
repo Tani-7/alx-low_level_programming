63518e91279e23dd56082207ebc39856a34ad99e
Tani-7 <justomwansa@outlook.com>
2022-09-22 05:01:17 -0700

    C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  *	main - Entry point
  *
- *	Description: prints "Programming is like building a multilingual puzzle
+ *	Description: prints "Programming is like building a multilingual puzzle"
  *
  *	Return: Always 0 (Success)
  */
@@ -12,6 +12,7 @@ int main(void)
 
 
 {	
+
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
