a613fbe84a52dfc1f80eed50620db1cd2b934251
Tani-7 <justomwansa@outlook.com>
2022-09-22 05:19:14 -0700

    C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,13 +4,13 @@
  * main - entry point
  *
  * Use of printf instead of puts
- * 
+ *
  * Return: 0 for a successful  exe
 */
 
 int main(void)
 
 {
-	printf("%s","with proper grammar, but the outcome is a piece of art,\n");
-	return(0)
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
